e35a1dad3da2acd6c8a784d50b2f4e60c9e1ebc9
sali <sali2@loyalty.com>
2015-11-30 20:47:46 -0500

    fix combo box commit

--- src/main/java/com/alphasystem/app/sarfengine/ui/SarfEnginePane.java
+++ src/main/java/com/alphasystem/app/sarfengine/ui/SarfEnginePane.java
@@ -7,17 +7,17 @@ import com.alphasystem.app.sarfengine.ui.control.model.TableModel;
 import com.alphasystem.arabic.model.NamedTemplate;
 import com.alphasystem.sarfengine.xml.model.*;
 import javafx.collections.ObservableList;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Group;
 import javafx.scene.Node;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.ComboBoxTableCell;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.KeyCodeCombination;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
+import javafx.stage.Screen;
 
 import java.util.List;
 
@@ -29,6 +29,9 @@ import static javafx.geometry.NodeOrientation.RIGHT_TO_LEFT;
 import static javafx.scene.control.ContentDisplay.GRAPHIC_ONLY;
 import static javafx.scene.control.ScrollPane.ScrollBarPolicy.AS_NEEDED;
 import static javafx.scene.control.SelectionMode.SINGLE;
+import static javafx.scene.control.cell.CheckBoxTableCell.forTableColumn;
+import static javafx.scene.input.KeyCode.D;
+import static javafx.scene.input.KeyCombination.CONTROL_DOWN;
 import static javafx.scene.text.TextAlignment.CENTER;
 
 /**
@@ -36,25 +39,61 @@ import static javafx.scene.text.TextAlignment.CENTER;
  */
 public class SarfEnginePane extends BorderPane {
 
+    private final TableView<TableModel> tableView;
+
     @SuppressWarnings({"unchecked"})
     public SarfEnginePane(ConjugationTemplate conjugationTemplate) {
         if (conjugationTemplate == null) {
             conjugationTemplate = new ConjugationTemplate();
         }
 
+        Screen screen = Screen.getPrimary();
+        Rectangle2D bounds = screen.getVisualBounds();
+        double boundsWidth = bounds.getWidth();
+        double largeColumnWidth = boundsWidth * 20 / 100;
+        double smallColumnWidth = boundsWidth * 9 / 100;
+
         ObservableList<TableModel> tableModels = observableArrayList();
         List<ConjugationData> dataList = conjugationTemplate.getData();
         dataList.forEach(data -> tableModels.add(new TableModel(data)));
-        tableModels.add(new TableModel());
-        TableView<TableModel> tableView1 = new TableView<>(tableModels);
-        tableView1.getSelectionModel().setSelectionMode(SINGLE);
-        tableView1.setPrefSize(1000, 640);
-        tableView1.setEditable(true);
+        tableView = new TableView<>(tableModels);
+        tableView.getSelectionModel().setSelectionMode(SINGLE);
+        tableView.setPrefSize(boundsWidth, 640);
+        tableView.setEditable(true);
+        initializeTable(largeColumnWidth, smallColumnWidth);
 
+        ScrollPane scrollPane = new ScrollPane(tableView);
+        scrollPane.setVbarPolicy(AS_NEEDED);
+        scrollPane.setHbarPolicy(AS_NEEDED);
+
+        setCenter(scrollPane);
+
+        MenuItem menuItem = new MenuItem("Debug");
+        menuItem.setOnAction(event -> {
+            ObservableList<TableModel> items = tableView.getItems();
+            items.forEach(tableModel -> {
+                RootLetters rootLetters = tableModel.getRootLetters();
+                System.out.println(format("Template: %s, Root Letters: %s %s %s", tableModel.getTemplate(),
+                        rootLetters.getFirstRadical().toCode(), rootLetters.getSecondRadical().toCode(),
+                        rootLetters.getThirdRadical().toCode()));
+            });
+        });
+        menuItem.setAccelerator(new KeyCodeCombination(D, CONTROL_DOWN));
+
+        MenuBar menuBar = new MenuBar();
+
+        Menu menu = new Menu("File");
+        menu.getItems().add(menuItem);
+        menuBar.getMenus().add(menu);
+        setTop(menuBar);
+    }
+
+    @SuppressWarnings("unchecked")
+    private void initializeTable(double largeColumnWidth, double smallColumnWidth) {
         // start adding columns
         TableColumn<TableModel, RootLetters> rootLettersColumn = new TableColumn<>();
-        rootLettersColumn.setText("Root\nLetters");
-        rootLettersColumn.setPrefWidth(200);
+        rootLettersColumn.setText("Root Letters");
+        rootLettersColumn.setPrefWidth(largeColumnWidth);
         rootLettersColumn.setEditable(true);
         rootLettersColumn.setCellValueFactory(new PropertyValueFactory<>("rootLetters"));
         rootLettersColumn.setCellFactory(RootLettersTableCell::new);
@@ -62,7 +101,7 @@ public class SarfEnginePane extends BorderPane {
         TableColumn<TableModel, NamedTemplate> templateColumn = new TableColumn<>();
         templateColumn.setText("Form");
         templateColumn.setEditable(true);
-        templateColumn.setPrefWidth(200);
+        templateColumn.setPrefWidth(largeColumnWidth);
         templateColumn.setCellValueFactory(new PropertyValueFactory<>("template"));
 
         templateColumn.setCellFactory(column -> new ComboBoxTableCell<TableModel, NamedTemplate>(NamedTemplate.values()) {
@@ -81,12 +120,8 @@ public class SarfEnginePane extends BorderPane {
                 labelText.setTextAlignment(CENTER);
                 labelText.setFont(Global.ENGLISH_FONT);
 
-                comboBox.focusedProperty().addListener((o, ov, nv) -> {
-                    if (nv) {
-                        getTableView().edit(getIndex(), getTableColumn());
-                    } else {
-                        commitEdit(comboBox.getSelectionModel().getSelectedItem());
-                    }
+                comboBox.getSelectionModel().selectedItemProperty().addListener((o, ov, nv) -> {
+                    commitEdit(nv);
                 });
             }
 
@@ -120,15 +155,15 @@ public class SarfEnginePane extends BorderPane {
         });
 
         TableColumn<TableModel, ObservableList<VerbalNoun>> verbalNounsColumn = new TableColumn<>();
-        verbalNounsColumn.setText("Verbal\nNouns");
-        verbalNounsColumn.setPrefWidth(200);
+        verbalNounsColumn.setText("Verbal Nouns");
+        verbalNounsColumn.setPrefWidth(largeColumnWidth);
         verbalNounsColumn.setEditable(true);
         verbalNounsColumn.setCellValueFactory(new PropertyValueFactory<>("verbalNouns"));
         verbalNounsColumn.setCellFactory(VerbalNounTableCell::new);
 
         TableColumn<TableModel, ObservableList<NounOfPlaceAndTime>> adverbsColumn = new TableColumn<>();
         adverbsColumn.setText("Adverbs");
-        adverbsColumn.setPrefWidth(200);
+        adverbsColumn.setPrefWidth(largeColumnWidth);
         adverbsColumn.setEditable(true);
         adverbsColumn.setCellValueFactory(new PropertyValueFactory<>("adverbs"));
         adverbsColumn.setCellFactory(AdverbTableCell::new);
@@ -138,6 +173,7 @@ public class SarfEnginePane extends BorderPane {
             NamedTemplate newValue = event.getNewValue();
             TableView<TableModel> tableView = event.getTableView();
             TableModel selectedItem = tableView.getSelectionModel().getSelectedItem();
+            selectedItem.setTemplate(newValue);
 
             // TODO: figure out how to update table
             List<VerbalNoun> verbalNouns = Global.VERBAL_NOUN_TEMPLATE_MAPPING.get(newValue);
@@ -154,12 +190,22 @@ public class SarfEnginePane extends BorderPane {
             }
         });
 
-        tableView1.getColumns().addAll(rootLettersColumn, templateColumn, verbalNounsColumn, adverbsColumn);
-        ScrollPane scrollPane = new ScrollPane(tableView1);
-        scrollPane.setVbarPolicy(AS_NEEDED);
-        scrollPane.setHbarPolicy(AS_NEEDED);
+        TableColumn<TableModel, Boolean> removePassiveLineColumn = new TableColumn<>();
+        removePassiveLineColumn.setText("Remove Passive Line");
+        removePassiveLineColumn.setPrefWidth(smallColumnWidth);
+        removePassiveLineColumn.setEditable(true);
+        removePassiveLineColumn.setCellValueFactory(new PropertyValueFactory<>("removePassiveLine"));
+        removePassiveLineColumn.setCellFactory(forTableColumn(removePassiveLineColumn));
 
-        setCenter(scrollPane);
+        TableColumn<TableModel, Boolean> skipRuleProcessingColumn = new TableColumn<>();
+        skipRuleProcessingColumn.setText("Skip Rule Processing");
+        skipRuleProcessingColumn.setPrefWidth(smallColumnWidth);
+        skipRuleProcessingColumn.setEditable(true);
+        skipRuleProcessingColumn.setCellValueFactory(new PropertyValueFactory<>("skipRuleProcessing"));
+        skipRuleProcessingColumn.setCellFactory(forTableColumn(skipRuleProcessingColumn));
+
+        tableView.getColumns().addAll(rootLettersColumn, templateColumn, verbalNounsColumn, adverbsColumn,
+                removePassiveLineColumn, skipRuleProcessingColumn);
     }
 
 }
